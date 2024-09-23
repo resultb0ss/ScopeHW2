469911bd4051da8b72edad275639ed51adaa7faa
Admin <reteamboss@gmail.com>
2024-09-23 14:09:24 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello World!")
+
+    val moscow = City("Москва")
+    moscow.setPopulation(1000000)
+    println("${moscow.name} с населением ${moscow.population}")
+}
+
+data class City(var name: String, var population: Int = 0){
+    fun setPopulation(_population: Int): City = apply { population = _population }
 }
